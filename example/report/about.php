307ecc182bb17f89f90f97ae0f85bcbe311e8e98
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-06-21 13:11:34 +0200

    Prepare to release htmlcss exercise

---
+++ example/report/about.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>Redovisning av kursmoment i kursen webtec</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+</head>
+
+<body>
+
+
+
+<header>
+    <nav>
+        <a href="index.php">Rapport</a> |
+        <a href="about.php">Om</a>
+    </nav>
+</header>
+
+
+
+<article>
+
+<h1>Om</h1>
+
+
+
+<h2>Om kursen</h2>
+
+<p>Denna webbplatsen är min rapportsida för kursen <a href="https://dbwebb.se/kurser/webtec">webtec</a>.</p>
+
+<p>Kursrepot för <a href="https://github.com/dbwebb-se/webtec">webtec finns på GitHub</a>.</p>
+
+<p>Vad sägs om att du lägger till en fin representativ bild för denna kursen? Välj själv vad som är representativt.</p>
+
+<p><img src="img/leaf_256x256.png" alt="Bild på dbwebb löv"></p>
+
+
+
+<h2>Om mig själv</h2>
+
+<p><img src="img/me.jpg" width="500" alt="Bild på mig"></p>
+
+<p>Så, en presentation en bra början. Skriv några ord om dig själv. Jag börjar.</p>
+
+<p>Mitt namn är Mikael Roos. Född och uppvuxen i Bankeryd, Småland, strax utanför Jönköping, i ett villaområde som byggdes upp samtidigt som vi flyttade in där. Jag gillade landhockey och har spelat bandy och hockey samt gått bowlinggymnasiet i Nässjö. Jag har varit städare, diskare, kallskänka, servitör och kock. På ett bananskal landade jag i Ronneby när jag började högskolan 1990 där och nu är jag kvar med fru, barn och så vidare.</p>
+
+<p>Programmering har alltid intresserat mig sedan 13-årsåldern och min första dator var en Spectravideo 328 med bandspelare.</p>
+
+<p>Om jag skall nämna någon hobby, förutom webbprogrammering, så får det bli att
+bära sten på sommarstugetomten, och det finns sten så det räcker och blir över.</p>
+
+<p>Till och från får jag för mig att börja på lite hobbies, ett år
+satsade jag på pokerspel, ett annat år var det geocaching och sedan turfing. Nu ligger jag lågt med hobbies, men något kanske dyker upp...?</p>
+
+<p>Vi syns och hörs i forum och chatt!</p>
+
+</article>
+
+
+
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ example/report/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>Redovisning av kursmoment i kursen webtec</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+</head>
+
+<body>
+
+
+
+<header>
+    <nav>
+        <a href="index.php">Rapport</a> |
+        <a href="about.php">Om</a>
+    </nav>
+</header>
+
+
+
+<article>
+
+<header>
+<h1>Redovisning av kursmoment i kursen webtec</h1>
+</header>
+
+<h2>Kmom01/02</h2>
+
+<p>Här är redovisningstexten för kursmomentet och dess uppgifter. Se till att skapa nya underrubriker för varje uppgift som skall redovisas.</p>
+
+<h3>Uppgift rapportsida</h3>
+
+<p>Besvara de frågor som fanns i samband med uppgiften.</p>
+
+<h3>Uppgift One-page-website</h3>
+
+<p>Besvara de frågor som fanns i samband med uppgiften.</p>
+
+<h3>Uppgift Webbsida med HTML &amp; CSS</h3>
+
+<p>Besvara de frågor som fanns i samband med uppgiften.</p>
+
+<h3>Avslutande ord</h3>
+
+<p>Besvara de frågor som fanns i slutet av kursmomentet.</p>
+
+
+
+<h2>Kmom03/04</h2>
+<p>Här är redovisningstexten</p>
+
+
+
+<h2>Kmom05/06</h2>
+<p>Här är redovisningstexten</p>
+
+
+
+<h2>Kmom07/10</h2>
+<p>Här är redovisningstexten</p>
+
+</article>
+
+
+
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
