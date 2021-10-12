f1d67bf95758953dffee304ed251573f55649dc3
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-10-12 10:51:18 +0200

    Add test for proj and kmom10

---
+++ .dbwebb/test/suite.d/proj_/100_me-proj_database-permissions.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+. ".dbwebb/inspect-src/kmom.d/functions.bash"
+
+TARGET_DIR="me/proj"
+doLog 0 "$TARGET_DIR: Always passes - not yet implemented"
+
+
+# EXECUTE_PHP="../../.dbwebb/test/suite.d/php_/.php/check_functions_exists.php"
+#
+# cd $TARGET_DIR || exit 1
+#
+# res=$( php $EXECUTE_PHP )
+# success=$?
+#
+# echo "$res" | sed '$d'
+#
+# doLog $success "$TARGET_DIR: functions exists $( echo "$res" | tail -n1 )"
