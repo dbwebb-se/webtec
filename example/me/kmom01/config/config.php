dbaed4e043c5e3fb8d39cc5192d2b6180bb249e2
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-06-29 10:15:36 +0200

    Added kmom01 me in example

---
+++ example/me/kmom01/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Report all type of errors
+error_reporting(-1);
+
+// Display all errors
+ini_set('display_errors', '1');
---
+++ example/me/kmom01/public/about.php
@@ -0,0 +1,14 @@
+<?php
+include('../config/config.php');
+
+$title = 'Om kurs och webbplatsen';
+
+include('../view/header.php');
+?>
+
+<main>
+    <h1>Om kursen och webbplatsen</h1>
+    <p>Här tänkte jag skriva lite om denna webbplatsen och om kursen.</p>
+</main>
+
+<?php include('../view/footer.php') ?>
