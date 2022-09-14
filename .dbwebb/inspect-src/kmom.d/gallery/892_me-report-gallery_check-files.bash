d9297ccf975aa4793db74bc02feb0ef411a3295f
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-09-14 23:05:13 +0200

    Prepare gui for kmom04

---
+++ .dbwebb/inspect-src/kmom.d/gallery/892_me-report-gallery_check-files.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+. ".dbwebb/inspect-src/kmom.d/functions.bash"
+
+TARGET_DIR="me/report"
+CHECK_FILES="
+public/session.php
+public/photocal.php
+public/guessname.php
+src/functions.php
+src/calendar.php
+"
+
+cd $TARGET_DIR || exit 1
+
+all=0
+fail=0
+for file in $CHECK_FILES; do
+    (( all++ ))
+    if [[ ! -f "$file" ]]; then
+        printf "Missing file '$file'.\n"
+        (( fail++ ))
+    fi
+done
+
+doLog $fail "$TARGET_DIR: check files ("$(( all-fail ))"/$all)"
