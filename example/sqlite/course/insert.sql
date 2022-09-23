86c799625597773271781ebf629102e67fe532ea
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-09-23 10:56:14 +0200

    Prepare kmom05

--- example/sqlite/course/insert.sql
+++ example/sqlite/course/insert.sql
@@ -3,7 +3,7 @@
 --
 INSERT INTO course
 VALUES (
-    'DV1462', 'htmlphp', 7.5, 1, NULL, NULL
+    'DV1462', 'webtec', 7.5, 1, NULL, NULL
 );
 
 INSERT INTO course 
@@ -27,7 +27,7 @@ VALUES
 -- INSERT INTO course
 --     (code, name, term)
 -- VALUES
---     ('PA1439', 'htmlphp', 1),
+--     ('PA1439', 'webtec', 1),
 --     ('DV1531', 'python', 1),
 --     ('PA1436', 'design', 2),
 --     ('DV1561', 'javascript', 2)
