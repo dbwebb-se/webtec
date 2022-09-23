b121af7f7fada7d95ebdbb8bcddb719739dfc1e9
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-09-23 12:32:25 +0200

    Add function returning all details of namedays

--- example/database/src/calendar.php
+++ example/database/src/calendar.php
@@ -5,6 +5,30 @@
  */
 
 /**
+ * Get the name days "namnsdagar" enligt Svenska akademin.
+ *
+ * @return array where the key is the name and the value is the date.
+ */
+function getNameDaysByName(): array
+{
+    $filename = "../data/Svenska_akademin_namnlista.csv";
+    $nameDays = [];
+    $handle = fopen($filename, "r");
+    if ($handle !== false) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+            $key = $data[0];
+            $value = $data[1];
+            $nameDays[$key] = $value;
+        }
+        fclose($handle);
+    }
+
+    return $nameDays;
+}
+
+
+
+/**
  * Get the name days "namnsdagar" enligt Svenska akademin, include all details
  * where the each key contain an array with details.
  *
